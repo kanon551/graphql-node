1ba4eee36ce9d46d7060ace3b566fd686d9078f7
vivek <kanon.m551@gmail.com>
2024-03-07 13:12:10 +0530

    recursion resolved

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -19,8 +19,9 @@ const createTask = async(title, description, due_date, assignee) => {
             });
 
             const result = await event.save();
+            const cretor = await getUser(result._doc.creator)
 
-            createdEvnt = { ...result._doc, _id: event._doc._id.toString() };
+            createdEvnt = { ...result._doc, _id: event._doc._id.toString(), creator: cretor };
 
             const registeredUser =  await User.findById('65e9425f976140ac30e4209f');
 
@@ -98,15 +99,47 @@ const saveUser = async(email, password) => {
     }
 }
 
-const getUser = async(userID) => {
+const getUser = async(userID, depth = 0) => {
     try{
-        const user =  await User.findById(userID);
+        if (depth >= 1) {
+            // Stop recursion after one level
+            return null;
+        }
 
-        return { ...user._doc, _id: user.id };
+        const user =  await User.findById(userID);
+        const newTasks = await events(user._doc.createdTasks, depth + 1)
+
+        return { ...user._doc, 
+                _id: user.id,
+                createdTasks: newTasks
+             };
          
     }
     catch(err){
         logger.error(`getUser -> ${err.message}`);
+        ExceptionResolver(err, null);
+    }
+}
+
+const events = async(eventIDs, depth = 0) => {
+    try{
+        const events = await Event.find({_id: {$in: eventIDs}});
+
+            const eventsWithUsers = await Promise.all(
+                events.map(async (event) => {
+                    const obtainedUser = await getUser(event.creator, depth + 1);
+                    return {
+                        ...event._doc,
+                        _id: event.id,
+                        creator: obtainedUser,
+                    };
+                })
+            );
+
+            return eventsWithUsers;
+    }
+    catch(err){
+        logger.error(`events -> ${err.message}`);
         ExceptionResolver(err, null);
     }
 }
